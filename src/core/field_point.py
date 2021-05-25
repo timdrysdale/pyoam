73f4794a5d70bbc798214c488794f4692499a6cc
Tim Drysdale <timothy.d.drysdale@gmail.com>
2021-05-25 01:45:18 +0100

    Add FieldPoint

---
+++ src/core/field_point.py
@@ -0,0 +1,20 @@
+"""FieldPoint is a ComplexPoint with a zero initial value
+
+The complex value is expected to use to store the magnitide and phase of an 
+electric field, which is not known at the time the FieldPoint is created.
+
+    Typical Usage:
+        x = 1
+        y = 2
+        z = 3
+        f1 = Field(x,y,z)
+        f1.add(1+2j)
+        assert(f1.real()==1)
+
+"""
+from complex_point import ComplexPoint
+
+class FieldPoint(ComplexPoint):
+    
+    def __init__(self, x,y,z):
+        ComplexPoint.__init__(self,x,y,z,0)
