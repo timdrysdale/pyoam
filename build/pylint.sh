a12eb66e8b2723e1a2170346915dfc5a8c20287e
Tim Drysdale <timothy.d.drysdale@gmail.com>
2021-05-27 12:06:44 +0100

    Fix pylint script re imports

--- build/pylint.sh
+++ build/pylint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-pylint --good-names=A,f,i,k,r,v,x,y,z --variable-rgx='^[a-z][a-z0-9]*((_[a-z0-9]+)*)?$' --argument-rgx='^[a-z][a-z0-9]*((_[a-z0-9]+)*)?$' ../src
+cd  ../src
+pylint --good-names=A,f,i,k,r,v,x,y,z --variable-rgx='^[a-z][a-z0-9]*((_[a-z0-9]+)*)?$' --argument-rgx='^[a-z][a-z0-9]*((_[a-z0-9]+)*)?$' ./core
+cd ../build
 
